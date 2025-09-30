ed389d8e48ed99d94ff064765a9e69a2c75642af
Rishabjs03 <rishabagarwaljs@gmail.com>
2025-09-30 23:06:32 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Build ke time ESLint errors ko ignore kare
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
